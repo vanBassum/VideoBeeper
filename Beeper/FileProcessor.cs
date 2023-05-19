bc6df216c65c65e8e04ebefe813b1d5859691bc0
Bas Visscher <bas.visscher24@gmail.com>
2023-05-19 22:13:38 +0200

    Fixed some problems

--- Beeper/FileProcessor.cs
+++ Beeper/FileProcessor.cs
@@ -2,6 +2,9 @@
 using NAudio.Wave;
 using FFmpeg.NET;
 using FFMpegCore;
+using FFMpegCore.Helpers;
+using FFMpegCore.Enums;
+using System.Diagnostics;
 
 namespace Beeper
 {
@@ -17,20 +20,29 @@ namespace Beeper
         public async void Start(Settings settings, string inputFile, string outputFile, CancellationToken token = default)
         {
             Name = Path.GetFileNameWithoutExtension(inputFile);
-            string audioFile = outputFile + ".wav";
+            string audioFile = outputFile + ".mp3";
             var mediaInfo = await FFProbe.AnalyseAsync(inputFile, null, token);
             await GenerateAudioAsync(settings, audioFile, mediaInfo.Duration);
-            var ffmpeg = new Engine("ffmpeg.exe");
-            ffmpeg.Progress += Ffmpeg_Progress;
-            await ffmpeg.ExecuteAsync($"-i {inputFile} -i {audioFile} -c:v copy -map 0:v:0 -map 1:a:0 {outputFile}", token);
+
+            FFMpegHelper.ConversionSizeExceptionCheck(FFProbe.Analyse(inputFile));
+
+            await FFMpegArguments
+                .FromFileInput(inputFile)
+                .AddFileInput(audioFile)
+                .OutputToFile(outputFile, overwrite: true, options=>options
+                    .CopyChannel(Channel.Video)
+                    //.WithAudioCodec(AudioCodec.Aac)
+                    //.WithAudioBitrate(AudioQuality.Good)
+                    .UsingShortest(false))
+                .NotifyOnProgress(a => Progress(a, mediaInfo.Duration))
+                .ProcessAsynchronously();
             File.Delete(audioFile);
             Status = "Done";
         }
 
-        private void Ffmpeg_Progress(object? sender, FFmpeg.NET.Events.ConversionProgressEventArgs e)
+        void Progress(TimeSpan progress, TimeSpan total)
         {
-            double p = (double)e.ProcessedDuration.TotalMinutes * 100 / (double)e.MediaInfo.TotalDuration.TotalSeconds;
-
+            double p = (double)progress.TotalSeconds * 100 / (double)total.TotalSeconds;
             if (p < 0 || p > 100)
             {
                 ctx.InvokeIfRequired(() =>
@@ -56,8 +68,20 @@ namespace Beeper
                 ISampleProvider silenceProvider = new SilenceProvider(signalGenerator.WaveFormat).ToSampleProvider();
 
                 TimeSpan t = settings.Offset;
-                ISampleProvider sampleProvider = silenceProvider.Take(settings.Offset);
+                ISampleProvider sampleProvider;
                 bool doBeep = true;
+
+                if (settings.Offset > TimeSpan.Zero)
+                {
+                    sampleProvider = silenceProvider.Take(settings.Offset);
+                    doBeep = true;
+                }
+                else
+                {
+                    sampleProvider = signalGenerator.Take(settings.Duration);
+                    doBeep = false;
+                }
+
                 while (t < length)
                 {
                     if (doBeep)
@@ -67,15 +91,26 @@ namespace Beeper
                     }
                     else
                     {
-                        if (t + settings.Interval < length)
-                        {
+                        if(t + settings.Interval < length)
                             sampleProvider = sampleProvider.FollowedBy(silenceProvider.Take(settings.Interval));
-                        }
                         t += settings.Interval;
                     }
                     doBeep = !doBeep;
                 }
-                WaveFileWriter.CreateWaveFile16(outputFile, sampleProvider);
+                // optionally truncate to video length
+                //var truncated = sampleProvider.Take(length);
+
+                // go back down to 16 bit PCM 
+                var converted16Bit = new SampleToWaveProvider16(sampleProvider);
+                // now for MP3, we need to upsample to 44.1kHz. Use MediaFoundationResampler 
+                using (var resampled = new MediaFoundationResampler(converted16Bit, new WaveFormat(44100, 1)))
+                {
+                    var desiredBitRate = 0; // ask for lowest available bitrate 
+                    MediaFoundationEncoder.EncodeToMp3(resampled, outputFile, desiredBitRate);
+                }
+
+
+                //WaveFileWriter.CreateWaveFile16(outputFile, converted16Bit);
             });
         }
 
